cef902ac91c87b46f472d6f255a9add6652fd663
Dan Fulcher <me@danfulcher.co.uk>
2018-11-21 00:08:14 +0000

    Added form and basic change calculator function.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form name="change-calc" @submit.prevent="calcChange(amount)">
+      <input v-model="amount" placeholder="Amount"/>
+      <button type="submit">Calculate</button>
+      {{ this.output }}
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data: function () {
+      return {
+          amount: '',
+          coins: [
+              200,
+              100,
+              50,
+              20,
+              10,
+              5,
+              2,
+              1
+          ],
+          output: []
+      }
+  },
+  methods: {
+      calcChange (amount) {
+        if (amount === 0)
+        {
+            return [];
+        }
+        else
+        {
+            // If given amount is larger than largest coin, removes largest coin value from amount and adds to the outputted array.
+            // Then runs the function again with leftover amount.
+            if (amount >= this.coins[0])
+            {
+                let left = (amount - this.coins[0]);
+                this.output.push(this.coins[0]);
+                return [this.coins[0]].concat( this.calcChange(left));
+            }
+            // If given amount is NOT larger the largest coin, it removes the largest coin from the array
+            // Then runs array again.
+            else
+            {
+                this.coins.shift();
+                return this.calcChange(amount);
+            }
+        }
+
+      }
   }
 }
 </script>
